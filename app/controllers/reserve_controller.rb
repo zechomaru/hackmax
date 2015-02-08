4ba89656c0b2f99766bd948b5a87e0bb16fc9d45
xavi <xavier457@hotmail.com>
2015-02-08 15:54:50 -0430

    status o

--- app/controllers/reserve_controller.rb
+++ app/controllers/reserve_controller.rb
@@ -1,42 +1,43 @@
 class ReserveController < ApplicationController
-	def index
+  helper_method :ocupado
+  
+  def index
 
-	end
-	def show
-		@show = seats params[:id]
+  end
+  def show
+    @show_id = params[:id]
+    @seats = seats @show_id
 
-		#theater = show.theater
-		#theater_id = theater.id
-		#@seats = Seat.where(theater_id: theater_id)		
-	end
-	def create
-		@reserve = Reserve.new
-		reserve_param = params[:reserve]
-		@movie.title = movie_param['title']
-		if @reserve.save
-		  redirect_to admin_movie_path
-		else
-		  render 'new'
-		end
-	end
-<<<<<<< HEAD
+    #theater = show.theater
+    #theater_id = theater.id
+    #@seats = Seat.where(theater_id: theater_id)    
+  end
+  def create
+    @reserve = Reserve.new
+    reserve_param = params[:reserve]
+    @movie.title = movie_param['title']
+    if @reserve.save
+      redirect_to admin_movie_path
+    else
+      render 'new'
+    end
+  end
 
-	def ocupado (seat_id, show_id)
-    reserve = Reserve.joins(:seat).where(seat_id: seat_id).where(show_id: show_id)
-    !reserve.empty?
-  	end
+  def ocupado(seat_id, show_id)
+      reserve = Reserve.where(seat_id: seat_id).where(show_id: show_id)
+      !reserve.empty?
+    end
 
-=======
-	def seats(id_show)
-		@show = Show.find(params[:id]).theater.seats
-	end
->>>>>>> c461b6bfe710791a73b604e918a2d710125f7c97
+  def seats(id_show)
+    @show = Show.find(params[:id]).theater.seats
+  end
+
 private
-	def check_available(id_show)
-		@show = Show.find(params[:id])
-		@show.theater.quantity
-	end
-	  def show_params
-	    params.require(:show).permit(:id)
-	  end
+  def check_available(id_show)
+    @show = Show.find(params[:id])
+    @show.theater.quantity
+  end
+    def show_params
+      params.require(:show).permit(:id)
+    end
 end
