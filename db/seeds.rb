494da3960c026b765ed033eb2f718839e52e9a8c
xavi <xavier457@hotmail.com>
2015-02-08 13:33:09 -0430

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#movie = Movie.create(title: "El Animal", genero: "Terror", lenguage: "Español", duration: "1:30", summary: "lalala lalalal lalal lala", classification: "A", subtitle: "Ninguno", ext: "test")
-#movie.save
-#theater= Theater.create(name: "Sala 1", quantity: 100)
-#theater.save
+movie = Movie.create(title: "El Animal", genero: "Terror", lenguage: "Español", duration: "1:30", summary: "lalala lalalal lalal lala", classification: "A", subtitle: "Ninguno", ext: "test")
+movie.save
+movie2 = Movie.create(title: "El Hach", genero: "Terror", lenguage: "Español", duration: "1:30", summary: "lalala lalalal lalal lala", classification: "A", subtitle: "Ninguno", ext: "test")
+movie2.save
+movie3 = Movie.create(title: "El Caballo", genero: "Terror", lenguage: "Español", duration: "1:30", summary: "lalala lalalal lalal lala", classification: "A", subtitle: "Ninguno", ext: "test")
+movie3.save
+theater= Theater.create(name: "Sala 1", quantity: 100)
+theater.save
+theater1= Theater.create(name: "VIP", quantity: 200)
+theater1.save
+
+#
